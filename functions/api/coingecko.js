7bdc5b561fa0711416d6933d81d8df821da7f5fc
Plus664 <soccer710a@gmail.com>
2026-02-06 16:21:21 +0900

    Add files via upload

--- functions/api/coingecko.js
+++ functions/api/coingecko.js
@@ -1,6 +1,12 @@
 export async function onRequest(context) {
-  const url = "https://api.coingecko.com/api/v3/coins/bitcoin?localization=false&tickers=false&market_data=true&community_data=false&developer_data=false&sparkline=false";
-  const res = await fetch(url);
+  const url = "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd,jpy&include_24hr_vol=true";
+
+  const res = await fetch(url, {
+    headers: {
+      "User-Agent": "btc-dashboard/1.0"
+    }
+  });
+
   const data = await res.json();
 
   return new Response(JSON.stringify(data), {
